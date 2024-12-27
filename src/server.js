83b97c5a9d8a54c87c7d81109c7f0da43a70f3e2
Dami <mailto:oficinadamianthames@gmail.com>
2024-12-27 11:20:02 -0300

    cambios cors

--- src/server.js
+++ src/server.js
@@ -4,8 +4,14 @@ const cors = require("cors");
 
 const server = express();
 
+// Configuración de CORS para permitir todas las solicitudes
+server.use(cors({
+  origin: '*', // Permitir todos los orígenes
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'] // Encabezados permitidos
+}));
+
 server.use(express.json());
-server.use(cors());
 
 server.use(router);
 
